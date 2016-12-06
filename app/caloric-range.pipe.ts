707d6acd04f5ae1d7c5a97f16f6db41b3961ef74
kf <Guest@Epicodus.local>
2016-12-05 21:41:16 -0500

    fix indentation on pipe file

--- app/caloric-range.pipe.ts
+++ app/caloric-range.pipe.ts
@@ -9,7 +9,7 @@ import { Food } from './food.model';
 export class CaloricRangePipe implements PipeTransform {
   transform(input: Food[], selectedCaloricRange)  {
     var output: Food[] = [];
-    var selectedCaloricRange = [ "-", "0-100", "101-400", "401-800", "801-1200", ">1200" ];
+    selectedCaloricRange = [ "-", "0-100", "101-400", "401-800", "801-1200", ">1200" ];
 
     console.log(input); console.log(selectedCaloricRange);
 
@@ -17,39 +17,63 @@ export class CaloricRangePipe implements PipeTransform {
 
       if (selectedCaloricRange === "0-100") {       //0-100
         for (var i = 0; i < input.length; i++) {
-          if (input[i].calories > 0 && input[i].calories <= 100) { output.push(input[i]); }
-          return output; }
+          if (input[i].calories > 0 && input[i].calories <= 100) {
+            output.push(input[i]);
+          }
+          return output;
+        }
         } else {
-          return input; } }
-
+          return input;
+        }
 
       if (selectedCaloricRange === "101-400") {       //101-400
         for (var i = 0; i < input.length; i++) {
-          if (input[i].calories > 100 && input[i].calories <= 400) { output.push(input[i]); }  return output; }
-        } else {
-      return input; } }
+          if (input[i].calories > 100 && input[i].calories <= 400) {
+            output.push(input[i]);
+          }
+          return output;
+        }
+      } else {
+        return input;
+      }
 
 
       if (selectedCaloricRange === "401-800") {       //401-800
         for (var i = 0; i < input.length; i++) {
-          if (input[i].calories > 400 && input[i].calories <= 800) { output.push(input[i]); }  return output; }
-        } else {
-      return input; } }
+          if (input[i].calories > 400 && input[i].calories <= 800) {
+            output.push(input[i]);
+          }
+          return output;
+        }
+      } else {
+        return input;
+      }
 
 
       if (selectedCaloricRange === "801-1200") {
         for (var i = 0; i < input.length; i++) {
-          if (input[i].calories > 800 && input[i].calories <= 1200) { output.push(input[i]); } } return output; }
-        } else {
-      return input; } }
-
+          if (input[i].calories > 800 && input[i].calories <= 1200) {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else {
+        return input;
+      }
+    } else {
       if (selectedCaloricRange === ">1200") {
         for (var i = 0; i < input.length; i++) {
-          if (input[i].calories > 1200) { output.push(input[i]); } } return output; }
-        } else {
-      return input; } } }
+          if (input[i].calories > 1200) {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else {
+        return input;
+      }
+    }
 
-    return output;
+    //return output;
   }
 }
 
